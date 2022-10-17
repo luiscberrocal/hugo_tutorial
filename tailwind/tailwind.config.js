7538b0fa8867281f8c514181a56ce4c3621b74fe
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-10-17 07:52:48 -0500

    WIP

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        extend: {
+            colors: {
+                brightRed: 'hsl(12, 88%, 59%)',
+            }
+        }
+    },
+    variants: {},
+    plugins: [],
+}
